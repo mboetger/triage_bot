180e9c71acb7680e3adb28c1007fcbfbaeb1b244
Matt Boetger <matt.boetger@gmail.com>
2026-07-06 14:05:16 -0700

    update gitignore

--- functions/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const calculateDailyBranchStatistics: import("firebase-functions/v2/scheduler").ScheduleFunction;
-export declare const getBranchStatistics: import("firebase-functions/v2/https").HttpsFunction;
-//# sourceMappingURL=index.d.ts.map
